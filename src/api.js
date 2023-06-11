cc10609ff57d788ec9bc64bd1c082a03748fd841
Lizzietrust <ohidaelizabeth3@gmail.com>
2023-06-11 18:40:44 +0100

    react api project for weather and forecast data

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '89ff8870cdmsh72d6a525790eaa2p10f5eejsn27b73502342d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "76d7314a5192869f99596c96c6849034";
